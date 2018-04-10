549c09313f91818fb4a3d07c2bb7d0dd698c23df
Robert Huselius <robert@huseli.us>
2018-04-10 13:10:44 +0200

    Added GetLetterGrade()

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
-﻿using GradeBook.Enums;
+﻿using System.Collections.Generic;
+using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
 {
@@ -6,6 +7,32 @@ namespace GradeBook.GradeBooks
     {
         public RankedGradeBook(string name) : base(name) {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade) {
+            double studentsPerGrade = Students.Count * 0.2;
+            List<double> averageGrades = new List<double>();
+            foreach (Student student in Students) {
+                averageGrades.Add(student.AverageGrade);
+            }
+            averageGrades.Sort();
+            //for (int i = averageGrades.Count - 1; i >= 0; i--) {
+            for (int i = 0; i < averageGrades.Count; i++) {
+                if (averageGrades[i] >= averageGrade) {
+                    double percentile = (double) i / (averageGrades.Count - 1);
+                    if (percentile >= 0.8)
+                        return 'A';
+                    else if (percentile >= 0.6)
+                        return 'B';
+                    else if (percentile >= 0.4)
+                        return 'C';
+                    else if (percentile >= 0.2)
+                        return 'D';
+                    else
+                        return 'F';
+                }
+            }
+            return 'A';
         }
     }
 }
